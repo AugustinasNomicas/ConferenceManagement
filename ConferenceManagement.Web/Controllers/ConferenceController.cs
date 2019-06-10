965d1708e3b72e76607f75742bb3856ad864bb29
Augustinas Nomicas <augustinas.nomicas@visma.com>
2019-06-10 11:14:11 +0300

    Add Conferences Controller

---
+++ ConferenceManagement.Web/Controllers/ConferenceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManagement.Web.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<ConferenceViewModel>()
+            {
+                new ConferenceViewModel() { IdConference = 1, Name ="Test", Description = "Desc"}
+            });
+        }
+    }
+}
---
+++ ConferenceManagement.Web/Models/ConferenceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceManagement.Web.Models
+{
+    public class ConferenceViewModel
+    {
+        [DisplayName("ID")]
+        public int IdConference { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

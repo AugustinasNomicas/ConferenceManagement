367ab91c00929451d488e5ecf784382f46ed9031
Augustinas Nomicas <augustinas.nomicas@visma.com>
2019-06-14 10:49:55 +0300

    Add ConferenceController integration test

---
+++ ConferenceManagement.Tests/IntegrationTests/Controllers/ConferenceControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using ConferenceManagement.Data.Repositories;
+using ConferenceManagement.Web.Controllers;
+using ConferenceManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace ConferenceManagement.Tests.IntegrationTests.Controllers
+{
+    public class ConferenceControllerTests : IntegrationTestBase
+    {
+        public ConferenceControllerTests() : base()
+        {
+
+        }
+
+        [Fact]
+        public void Controller_AddConference_ReturnsNewConferenceAsLast()
+        {
+            var repository = ServiceProvider.GetRequiredService<IConferenceRepository>();
+            var controller = new ConferenceController(repository);
+
+            // create new conference
+            var result = controller.Create(new Web.Models.ConferenceViewModel
+            {
+                Name = "test Conf",
+                Description = "hello"
+            }) as RedirectToActionResult;
+
+            Assert.Equal("Conference", result.ControllerName);
+            Assert.Equal("Index", result.ActionName);
+
+            // get last conference
+            var conferenceList = (controller.Index() as ViewResult).Model as IEnumerable<ConferenceViewModel>;
+            var conferenceViewModel = conferenceList.Last();
+
+            Assert.Equal("test Conf", conferenceViewModel.Name);
+            Assert.Equal("hello", conferenceViewModel.Description);
+        }
+    }
+}
---
+++ ConferenceManagement.Tests/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,26 @@
+﻿using ConferenceManagement.Data;
+using ConferenceManagement.Data.Repositories;
+using ConferenceManagement.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConferenceManagement.Tests.IntegrationTests
+{
+    public class IntegrationTestBase
+    {
+        protected ConferenceDbContext ConferenceDbContext;
+        protected IServiceProvider ServiceProvider;
+
+        public IntegrationTestBase()
+        {
+            var services = new ServiceCollection();
+
+            var startup = new Startup(null);
+
+            startup.ConfigureServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
